ad5ebcf072aaf7500bbbcea5630e0e8877e9ab99
jasonbhaas <jasonbhaas@gmail.com>
2016-03-31 14:27:50 -0400

    fixed models

--- myquiz/quiz/models.py
+++ myquiz/quiz/models.py
@@ -2,17 +2,26 @@ from django.db import models
 from django import forms
 
 
-# Create your models her
-
-# class Quiz(models.Model):
-#     subject = models.CharField(max_length= 50)
-#     name = models.CharField(max_length=30)
-#     description = models.TextField()
-#     instructions = models.TextField()
-#     author = models.ForeignKey(User)
+# Create your models here.
+# class Student(models.Model):
+#     username = models.CharField(max_length=30)
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#     password = forms.CharField(widget=forms.PasswordInput) #TODO will come back to this when looking at authentication the Django User Model
+#     email = models.EmailField(max_length=254) #TODO look into DJANGO email verification
     
 #     def __str__(self):
-# 	return self.subject + "-" + self.name
+# 	return self.username
+
+class Quiz(models.Model):
+    subject = models.CharField(max_length= 50)
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    instructions = models.TextField()
+    author = models.ForeignKey(User)
+    
+    def __str__(self):
+	return self.subject + "-" + self.name
 
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz)
